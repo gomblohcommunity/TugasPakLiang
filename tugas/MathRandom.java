95e3abac2561983d7a2268037e28b1fa6d244fcb
danang <danangwijaya750@gmail.com>
2018-11-13 11:37:59 +0700

    tugas

---
+++ tugas/MathRandom.java
@@ -0,0 +1,7 @@
+public class MathRandom{
+    public static void main (String[]args){
+        int randomNumber = (int)(Math.random() * 27);
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        System.out.println(Character.toUpperCase(alphabet.charAt(randomNumber)));
+    }
+}
