6881dbb216b3b19a39a77044f0ac75176c246681
danang <danangwijaya750@gmail.com>
2018-11-13 11:59:12 +0700

    asd

--- tugas/MathRandom.java
+++ tugas/MathRandom.java
@@ -1,7 +1,10 @@
 public class MathRandom{
     public static void main (String[]args){
-        int randomNumber = (int)(Math.random() * 27);
+        //. alphabet variable initiation
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        //. randomNumber variable initiation and get random number to hit the alphabet index
+        int randomNumber = (int)(Math.random() * 27);
+        //. print the uppercase alpahbet character after hit the index by randomNumber
         System.out.println(Character.toUpperCase(alphabet.charAt(randomNumber)));
     }
 }
--- tugas/TwoRact.java
+++ tugas/TwoRact.java
@@ -2,40 +2,46 @@ import java.io.*;
 public class TwoRact {
     public static void main(String[] args){
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));	
-try{
-        double pp1[]= new double[4];
-        double pp2[]= new double[4];
-		System.out.print("masukan Koordinat x Persegi panjang 1: ");
-		pp1[0] = Double.parseDouble(input.readLine());
-        System.out.print("masukan Koordinat y Persegi panjang 1: ");
-		pp1[1] = Double.parseDouble(input.readLine());
-        System.out.print("masukan Panjang Persegi panjang 1: ");
-		pp1[2] = Double.parseDouble(input.readLine());
-        System.out.print("masukan Tinggi x Persegi panjang 1: ");
-		pp1[3] = Double.parseDouble(input.readLine());
-        System.out.println("===============================================");
-		System.out.print("masukan Koordinat x Persegi panjang 2: ");
-		pp2[0] = Double.parseDouble(input.readLine());
-        System.out.print("masukan Koordinat y Persegi panjang 2: ");
-		pp2[1] = Double.parseDouble(input.readLine());
-        System.out.print("masukan Panjang Persegi panjang 2: ");
-		pp2[2] = Double.parseDouble(input.readLine());
-        System.out.print("masukan Tinggi Persegi panjang 2: ");
-		pp2[3] = Double.parseDouble(input.readLine());
+		try{
+			double pp1[]= new double[4];
+			double pp2[]= new double[4];
+			System.out.print("masukan Koordinat x Persegi panjang 1: ");
+			pp1[0] = Double.parseDouble(input.readLine());
+			System.out.print("masukan Koordinat y Persegi panjang 1: ");
+			pp1[1] = Double.parseDouble(input.readLine());
+			System.out.print("masukan Panjang Persegi panjang 1: ");
+			pp1[2] = Double.parseDouble(input.readLine());
+			System.out.print("masukan Tinggi x Persegi panjang 1: ");
+			pp1[3] = Double.parseDouble(input.readLine());
+			System.out.println("===============================================");
+			System.out.print("masukan Koordinat x Persegi panjang 2: ");
+			pp2[0] = Double.parseDouble(input.readLine());
+			System.out.print("masukan Koordinat y Persegi panjang 2: ");
+			pp2[1] = Double.parseDouble(input.readLine());
+			System.out.print("masukan Panjang Persegi panjang 2: ");
+			pp2[2] = Double.parseDouble(input.readLine());
+			System.out.print("masukan Tinggi Persegi panjang 2: ");
+			pp2[3] = Double.parseDouble(input.readLine());
 
+			String message= checking(pp1,pp2);
+			System.out.println(message);		
+		}catch(IOException ex){
+			System.out.println(ex.toString());
+		}
+    }
+	private static String checking(double[] pp1, double[] pp2){
+		String message="";
 		// Determine whether the second rectangle is inside the first
 		if	((Math.pow(Math.pow(pp2[1] - pp1[1], 2), .05) + pp2[3] / 2 <= pp1[3] / 2) && 
 			(Math.pow(Math.pow(pp2[0] - pp1[0], 2), .05) + pp2[2] / 2 <= pp1[2] / 2) &&
 			(pp1[3] / 2 + pp2[3] / 2 <= pp1[3]) &&
 			(pp1[2] / 2 + pp2[2] / 2 <= pp1[2]))
-			System.out.println("pp2 didalam pp1");
+			{message="pp2 didalam pp1";}
 		else if ((pp1[0] + pp1[2] / 2 > pp2[0] - pp2[2]) ||
 					(pp1[1] + pp1[3] / 2 > pp2[1] - pp2[3]))
-			System.out.println("r2 Memotong r1");
+			{message="r2 Memotong r1";}
 		else
-			System.out.println("r2 tidak memotong r1");
-    }catch(IOException ex){
-        System.out.println(ex.toString());
-    }
-    }
+			{message="r2 tidak memotong r1";}
+		return message;
+	}
 }
